2734e01ac8be9b30fcd98f8c6bf72d1a0372eec6
Janakdudhrejiya <47530864+Janakdudhrejiya@users.noreply.github.com>
2020-07-08 22:51:38 +0530

    DevOps

---
+++ myscript_Linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+set JAVA_HOME='/user/share/jdk'
+cd /src/main/gradle
+gradlew copyFiles
---
+++ myscript_Windows.bat
@@ -0,0 +1,6 @@
+@echo off
+
+D:
+set JAVA_HOME="C:\Program Files\Java\jre7"
+cd D:\src\copy_Project\DevOps
+start gradlew copyFiles
