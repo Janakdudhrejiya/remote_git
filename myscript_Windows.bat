6bb3fea41627d0a6a416ef2c6c358c504bb5a8fc
Janakdudhrejiya <47530864+Janakdudhrejiya@users.noreply.github.com>
2020-07-09 00:49:07 -0400

    Update myscript_Windows.bat
    update JAVA path

--- myscript_Windows.bat
+++ myscript_Windows.bat
@@ -1,6 +1,6 @@
 @echo off
 
 D:
-set JAVA_HOME="C:\Program Files\Java\jre7"
-cd D:\src\copy_Project\DevOps
-start gradlew copyFiles
+set JAVA_HOME="\\192.168.218.160\c$\Program Files\Java\jre1.8.0_211"
+cd D:\Janak\Copy_Task
+gradlew copyFiles
